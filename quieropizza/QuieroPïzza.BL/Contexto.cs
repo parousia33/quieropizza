6561444c9080f758dc56701a8285e8698e16e37e
Lopez <parousia33@gmail.com>
2020-02-16 21:04:28 -0600

    configurando el contexto

---
+++ quieropizza/QuieroPïzza.BL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuieroPïzza.BL
+{
+   public class Contexto: DbContext
+    {
+        public Contexto(): base("QuieroPizzaDB")
+        {
+
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
